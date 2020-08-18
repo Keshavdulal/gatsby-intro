b4634acdd24aad8967cca402ef24ac4e87f3612c
keshavdulal <keshav.dulal@gmail.com>
2020-08-18 16:55:39 +0545

    configure gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Frontend Masters Gatsby Workshop',
+    description: 'Full Day FM Gatsby Workshop',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
+  ],
 };
