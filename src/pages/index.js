91cc05a430d9e08a96e9e491b9a2d5bf77b142d8
keshavdulal <keshav.dulal@gmail.com>
2020-08-18 16:56:27 +0545

    master : added layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Layout from '../components/layout';
+import { css } from '@emotion/core';
 
-export default () => (
-  <Layout>
-    <div>Hello Frontend Masters!</div>
-    <Link to="/about">About Me</Link>
-  </Layout>
-);
+import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/post-preview';
+
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout
+      css={css`
+        border-bottom: 1px solid #ddd;
+        margin-top: 0.75rem;
+        padding-bottom: 1rem;
+        :first-of-type {
+          margin-top: 1rem;
+        }
+      `}
+    >
+      <div>Hello Frontend Masters!</div>
+      <Link to="/about">About Me</Link>
+
+      <h2>Read My Blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
