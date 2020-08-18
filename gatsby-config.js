7abf9d117e95c393e541734088d1eb9a8ac8eb80
keshavdulal <keshav.dulal@gmail.com>
2020-08-18 19:36:53 +0545

    master : add bundle analyzer

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,14 @@ module.exports = {
         path: 'posts',
       },
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
   ],
 };
