5f5817f16c35fc3b6821020adcc60ed1cdc45e1f
keshavdulal <keshav.dulal@gmail.com>
2020-08-18 16:54:15 +0545

    added gatsby node config to access mdx posts in diff dir

---
+++ gatsby-node.js
@@ -0,0 +1,32 @@
+// exposing gatsby-* apis to the dev env
+// node interface
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    // panic prints out error while stopping the build
+    reporter.panic('@panic-failed to create posts', result.errors);
+  }
+
+  const posts = result.data.allMdx.nodes;
+
+  posts.forEach(post => {
+    actions.createPage({
+      path: post.frontmatter.slug,
+      component: require.resolve('./src/templates/post.js'),
+      context: {
+        slug: post.frontmatter.slug,
+      },
+    });
+  });
+};
